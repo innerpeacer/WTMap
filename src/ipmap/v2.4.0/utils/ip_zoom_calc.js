2cba0055949c4d1dcd2d6ed6acf2f777f1174776
innerpeacer <innerpeacer@gmail.com>
2019-06-21 15:05:33 +0800

    BugFix: Fix the base zoom caculator, considering pixelRatio

--- src/ipmap/v2.4.0/utils/ip_zoom_calc.js
+++ src/ipmap/v2.4.0/utils/ip_zoom_calc.js
@@ -1,4 +1,4 @@
-let worldSize = 1;
+const worldSize = 1;
 
 function lngX(lng) {
     return (180 + lng) * worldSize / 360;
@@ -20,46 +20,47 @@ function yLat(y) {
 
 // maxBounds = [lng1, lat1, lng2, lat2]
 function getActualBounds(maxBounds, width, height) {
-    var pixels = [lngX(maxBounds[0]), latY(maxBounds[1]), lngX(maxBounds[2]), latY(maxBounds[3])];
+    let pixels = [lngX(maxBounds[0]), latY(maxBounds[1]), lngX(maxBounds[2]), latY(maxBounds[3])];
 
-    var pixelSize = {x: Math.abs(pixels[0] - pixels[2]), y: Math.abs(pixels[1] - pixels[3])};
-    var pixelCenter = {x: (pixels[0] + pixels[2]) * 0.5, y: (pixels[1] + pixels[3]) * 0.5};
+    let pixelSize = {x: Math.abs(pixels[0] - pixels[2]), y: Math.abs(pixels[1] - pixels[3])};
+    let pixelCenter = {x: (pixels[0] + pixels[2]) * 0.5, y: (pixels[1] + pixels[3]) * 0.5};
 
-    var pixelScaleX = width / pixelSize.x;
-    var pixelScaleY = height / pixelSize.y;
+    let pixelScaleX = width / pixelSize.x;
+    let pixelScaleY = height / pixelSize.y;
 
-    var actualPixels = [pixels[0], pixels[1], pixels[2], pixels[3]];
+    let actualPixels = [pixels[0], pixels[1], pixels[2], pixels[3]];
     if (pixelScaleX < pixelScaleY) {
-        var actualSizeX = pixelSize.y * width / height;
-        actualPixels[0]= pixelCenter.x - actualSizeX * 0.5;
-        actualPixels[2]= pixelCenter.x + actualSizeX * 0.5;
+        let actualSizeX = pixelSize.y * width / height;
+        actualPixels[0] = pixelCenter.x - actualSizeX * 0.5;
+        actualPixels[2] = pixelCenter.x + actualSizeX * 0.5;
     } else {
-        var actualSizeY = pixelSize.x * height / width;
+        let actualSizeY = pixelSize.x * height / width;
         actualPixels[1] = pixelCenter.y - actualSizeY * 0.5;
         actualPixels[3] = pixelCenter.y + actualSizeY * 0.5;
     }
-    return [xLng(actualPixels[0]), yLat(actualPixels[1]), xLng(actualPixels[2]),  yLat(actualPixels[3])];
+    return [xLng(actualPixels[0]), yLat(actualPixels[1]), xLng(actualPixels[2]), yLat(actualPixels[3])];
 }
 
 function calculateZoomForMaxBounds(maxBounds, width, height) {
-    var actualBounds = getActualBounds(maxBounds, width, height);
+    let actualBounds = getActualBounds(maxBounds, width, height);
     return calculateZoom(actualBounds, width, height);
 }
 
 function calculateZoom(bounds, width, height) {
     // console.log("calculate zoom");
-    var lng1 = bounds[0];
-    var lat1 = bounds[1];
-    var lng2 = bounds[2];
-    var lat2 = bounds[3];
+    let lng1 = bounds[0];
+    let lat1 = bounds[1];
+    let lng2 = bounds[2];
+    let lat2 = bounds[3];
 
-    var size_x = Math.abs(lng1 - lng2);
-    var size_y = Math.abs(lat1 - lat2);
+    let size_x = Math.abs(lng1 - lng2);
+    let size_y = Math.abs(lat1 - lat2);
 
-    var scaleX = width * 180 / size_x / 512;
-    var scaleY = height * 85.05113 * 2 / size_y / 512;
+    let pixelRatio = window.devicePixelRatio || 1;
+    let scaleX = width / pixelRatio * 180 / size_x / 512;
+    let scaleY = height / pixelRatio * 85.05113 * 2 / size_y / 512;
 
-    var zoom = scaleToZoom(Math.min(scaleX, scaleY)) + 1;
+    let zoom = scaleToZoom(Math.min(scaleX, scaleY)) + 1;
     return zoom;
 }
 
