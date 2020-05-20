d66ad7fe42f2aa7e1539d7c192fbb00719422d09
innerpeacer <innerpeacer@gmail.com>
2020-05-20 15:41:11 +0800

    Feature: Add trace filter and trace comparsion

--- html/tracker/trace_helper.js
+++ html/tracker/trace_helper.js
@@ -2,6 +2,8 @@ var http = new wtmap.HttpRequest();
 
 function createTraceLayer(name) {
     var traceLayer = new wtmap.CustomTraceLayer(name);
+    // traceLayer.setTextLayoutProperty("text-field", ["concat", "index: ", ["get", "index"]]);
+    traceLayer.setTextLayoutProperty("text-field", ["concat", ["get", "date"]]);
     return traceLayer;
 }
 
@@ -12,6 +14,8 @@ function tracePointsToGeojson(tracePoints) {
         var lp = new wtmap.LocalPoint(tp.x, tp.y, tp.floor);
         var lngLat = lp.getLngLat();
         lngLat.properties = {
+            index: i,
+            date: tp.date,
             floor: tp.floor
         };
         tpArray.push(lngLat);
@@ -20,7 +24,16 @@ function tracePointsToGeojson(tracePoints) {
 }
 
 var queryTraceUrl = "/LBServer/queryTrace";
+var queryFilteredTraceUrl = "/LBServer/queryFilteredTrace";
 
-function getQueryTraceUrl(userID) {
-    return queryTraceUrl + "?userID=" + userID;
+function getQueryTraceUrl(userID, isFilter) {
+    if (isFilter) {
+        return queryFilteredTraceUrl + "?distance=20&window=30&date=2020-05-20&userID=" + userID;
+        // return queryFilteredTraceUrl + "?distance=20&date=2020-05-19&userID=" + userID;
+    }
+    return queryTraceUrl + "?date=2020-05-20&userID=" + userID;
+}
+
+function getTestTraceUrl() {
+    return "http://101.132.138.146/locationServer/location/queryHistoryLocationOne?buildingId=05712001&peopleId=1&startTime=2020-05-15 09:50:17&endTime=2020-05-15 19:50:17&type=0";
 }
