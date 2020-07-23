9b565f2f74df2426c1294e0752f8b056d45e94b3
innerpeacer <innerpeacer@gmail.com>
2020-07-23 15:50:33 +0800

    Refactor: zip v2.3.1, v2.5.0 code

--- src/ipmap/v2.5.0/config/inherit.js
+++
@@ -1,6 +0,0 @@
-import BoxMap from "../../../mapbox/1.0.0/src/ui/map";
-import CacheVersion from "../../../mapbox/1.0.0/src/cache/cache_version"
-import TileCacheDB from "../../../mapbox/1.0.0/src/cache/tile_cache_db"
-import GlyphCacheDB from "../../../mapbox/1.0.0/src/cache/glyph_cache_db"
-
-export {BoxMap, CacheVersion, TileCacheDB, GlyphCacheDB}
